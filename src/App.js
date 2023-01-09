03179410e3118328b96e356edce0dc9e9133fc47
Petros Tsakonas <petros-tsakonas@hotmail.com>
2023-01-09 13:29:19 +0200

    Styling and Dynamic Data
    - Adding Basic CSS styling
    - Outputting Dynamic Data & Working with Expressions in JSX

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,27 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-   return <h2>Expense item!</h2>
+   const expenseDate = new Date();
+   const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+      weekday: 'short' // optional values: long/short/narrow
+   }
+   const expenseTitle = 'Car Insurance';
+   const expenseAmount = 298.67;
+
+   return (
+      <div className='expense-item'>
+         <div>{Intl.DateTimeFormat('en-UK', options).format(expenseDate)}</div>
+         <div className='expense-item__description'>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>{expenseAmount}</div>
+         </div>
+      </div>
+   );
 }
 
 export default ExpenseItem;
