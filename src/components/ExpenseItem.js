f7a306ccd972bdc8f49165aa3972887d856422f8
Petros Tsakonas <petros-tsakonas@hotmail.com>
2023-01-09 15:45:12 +0200

    Passing Data via "props"

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-   const expenseDate = new Date();
-   const options = {
-      hour: 'numeric',
-      minute: 'numeric',
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric',
-      weekday: 'short' // optional values: long/short/narrow
-   }
+function ExpenseItem(props) {
+   const expenseDate = new Date(2023, 0, 9);
    const expenseTitle = 'Car Insurance';
    const expenseAmount = 298.67;
 
    return (
       <div className='expense-item'>
-         <div>{Intl.DateTimeFormat('en-UK', options).format(expenseDate)}</div>
+         <div>{props.date.toISOString()}</div>
          <div className='expense-item__description'>
-            <h2>{expenseTitle}</h2>
-            <div className='expense-item__price'>{expenseAmount}</div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>{props.amount}</div>
          </div>
       </div>
    );
