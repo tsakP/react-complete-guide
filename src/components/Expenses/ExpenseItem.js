f9cc45b2fd3ad098264305633aee66e32667446f
Petros Tsakonas <petros-tsakonas@hotmail.com>
2023-01-10 16:08:14 +0200

    Components file organization and function syntax change to arrow

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,9 @@
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
    return (
       <Card className='expense-item'>
          <ExpenseDate date={props.date} />
