875b7210082f927c91b6939562d74b75b0cc3119
Petros Tsakonas <petros-tsakonas@hotmail.com>
2023-01-11 18:42:45 +0200

    Rendering Lists of Data Dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,35 +9,20 @@ const Expenses = (props) => {
    const [selectedYear, setSelectedYear] = useState('2022');
 
    const yearSelectedHandler = year => {
-      console.log('In Expenses.js');
       setSelectedYear(year);
-      console.log(selectedYear);
    }
 
    return (
       <div>
          <Card className='expenses'>
             <ExpensesFilter selected={selectedYear} onYearSelection={yearSelectedHandler} />
-            <ExpenseItem
-               title={props.data[0].title}
-               amount={props.data[0].amount}
-               date={props.data[0].date}
-            />
-            <ExpenseItem
-               title={props.data[1].title}
-               amount={props.data[1].amount}
-               date={props.data[1].date}
-            />
-            <ExpenseItem
-               title={props.data[2].title}
-               amount={props.data[2].amount}
-               date={props.data[2].date}
-            />
-            <ExpenseItem
-               title={props.data[3].title}
-               amount={props.data[3].amount}
-               date={props.data[3].date}
-            />
+            {props.data.map(expense => (
+               <ExpenseItem
+                  title={expense.title}
+                  amount={expense.amount}
+                  date={expense.date}
+               />
+            ))}
          </Card>
       </div>
    );
